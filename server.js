dddebf13aff33ac302927fc96b68399984f22f29
Koenrad MacBride <kbmacbride@gmail.com>
2016-11-10 09:43:30 -0800

    Initial upload

---
+++ server.js
@@ -0,0 +1,76 @@
+// MEAN Stack RESTful API Tutorial - Contact List App
+
+var express = require('express');
+var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']);
+var bodyParser = require('body-parser');
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+app.get('/contactlist', function (req, res) {
+  console.log('I received a GET request');
+  
+  db.contactlist.find(function (err, docs) {
+    console.log(docs);
+    res.json(docs);
+  });
+  /*person1 = {
+	  name:"Rick",
+	  email:"rick@roll.com",
+	  number:"111-111-1111"
+  };
+  person2 = {
+	  name:"Roy",
+	  email:"roy@roll.com",
+  number:"111-111-2222"
+  };
+  person3 = {
+	  name:"Ron",
+	  email:"ron@roll.com",
+	  number:"111-111-3333"
+  };
+  
+  var contactlist = [person1, person2, person3];
+  res.json(contactlst);
+  */
+});
+
+app.post('/contactlist', function (req, res) {
+  console.log(req.body);
+  db.contactlist.insert(req.body, function(err, doc) {
+    res.json(doc);
+  });
+});
+
+app.delete('/contactlist/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.contactlist.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+app.get('/contactlist/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+app.put('/contactlist/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(req.body.name);
+  db.contactlist.findAndModify({
+    query: {_id: mongojs.ObjectId(id)},
+    update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},
+    new: true}, function (err, doc) {
+      res.json(doc);
+    }
+  );
+});
+
+app.listen(3000);
+console.log("Server running on port 3000");
